c893967928325e617407467f50a30adcafdb9a8f
HennaS00 <60448391+HennaS00@users.noreply.github.com>
2020-04-02 17:12:49 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var submitButton = document.getElementById('buttonid');
+    submitButton.addEventListener('click', function(event) {
+        alert('THANK YOU FOR SUBMITTING, PAWESOME FRIEND!');
+})});
